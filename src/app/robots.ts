286003b145a9dec65d68c6059c5b010683689068
HarshaJyos <harshajyosyabhatla@gmail.com>
2026-02-18 19:15:18 +0530

    second deployment_robots.ts_updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    const baseUrl = 'https://jssr.vercel.app'; // Update with your actual domain
+    const baseUrl = 'https://jssr-eta.vercel.app'; // Update with your actual domain
 
     return {
         rules: {
